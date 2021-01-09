8350d916f8fbb71d5bdd8db481563741e39e7aa2
Antonio Andrade <aandrade@limasoft.pt>
2021-01-09 12:21:33 +0000

    add admin interface

--- resources/views/sad/dashboard.blade.php
+++ resources/views/sad/dashboard.blade.php
@@ -1,5 +1,22 @@
-@extends('layouts.flattern')
+@extends('layouts.sbadmin2')
+
+@section('pagetitle')
+    Dashboard
+@endsection
 
 @section('content')
-    <h1>SuperAdmin Dashboard</h1>
+    @include('sbadmin2parts.resume')
+    
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
+        </div>
+        <div class="card-body">
+            <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
+                CSS bloat and poor page performance. Custom CSS classes are used to create
+                custom components and custom utility classes.</p>
+            <p class="mb-0">Before working with this theme, you should become familiar with the
+                Bootstrap framework, especially the utility classes.</p>
+        </div>
+    </div>
 @endsection
